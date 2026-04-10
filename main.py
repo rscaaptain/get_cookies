707411c371f11e2e95e7e90a2874b548f2e9df98
RS CAPTAIn <psratul732@gmail.com>
2026-04-10 21:43:51 +0600

    Added TiDB database integration

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import undetected_chromedriver as uc
 import time
+import pymysql
 
 app = FastAPI()
 
-@app.get("/get-cookies")
-def get_cookies(url: str):
+@app.get("/")
+def update_cookies():
+    url = "https://shop.garena.my/"
     options = uc.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
@@ -20,5 +22,23 @@ def get_cookies(url: str):
     
     cookies = driver.get_cookies()
     driver.quit()
+
+    connection = pymysql.connect(
+        host='gateway01.eu-central-1.prod.aws.tidbcloud.com',
+        port=4000,
+        user='3A6TaGgYi7CaDM1.root',
+        password='tiy9rHALwhwfMLB5',
+        database='free_fire',
+        ssl={"reject_hostname": False}
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO cookies (name, value) VALUES (%s, %s) ON DUPLICATE KEY UPDATE value = VALUES(value)"
+            for cookie in cookies:
+                cursor.execute(sql, (cookie['name'], cookie['value']))
+        connection.commit()
+    finally:
+        connection.close()
     
-    return {"cookies": cookies}
+    return {"status": "success", "message": "Cookies successfully updated in database"}
